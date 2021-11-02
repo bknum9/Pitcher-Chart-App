4b9c7081dceb8f57ab1ea61bfedfcc361bbfdabf
bknum9 <92531634+bknum9@users.noreply.github.com>
2021-11-02 14:31:47 -0400

    recent updates 11/1/21

---
+++ play.js
@@ -0,0 +1,31 @@
+function incrementStrike(){
+    var element = document.getElementById('incrementStrike');
+    var value = element.innerHTML;
+
+    ++value;
+    if (value == 3){
+      window.clearInterval('incrementStrike');
+      value = 0;
+    }
+    console.log(value);
+    document.getElementById('incrementStrike').innerHTML = value;
+  }
+  function incrementBall(){
+    var element = document.getElementById('incrementBall');
+    var value = element.innerHTML;
+
+    ++value;
+    if (value == 4){
+      window.clearInterval('incrementBall');
+      value = 0;
+    }
+    console.log(value);
+    document.getElementById('incrementBall').innerHTML = value;
+  }
+  function togglePopup(){
+    document.getElementById("pitchinfopop").classList.toggle('active');
+  }
+
+  function content(elem) {
+    elem.style.backgroundColor = "red";
+  }
