ea02c56f04e664d2cf6a0ea03c540ec97794d5f4
Kevin Kelly <kevinclaytonkelly@gmail.com>
2021-11-04 02:23:35 -0400

    Add strike zone svg

--- modal_example/modal_example.js
+++ modal_example/modal_example.js
@@ -1,36 +1,47 @@
-function showModal(){
-        $('#pitch_type_modal').modal('show');
+function showModal() {
+    $('#pitch_type_modal').modal('show');
 }
-$(document).ready(function(){
-          $('#pitch_type') .dropdown({
-    action: function pitchDetailModal(text, value){
-        $('#pitch_detail_modal').modal('show');
-    },
-    placeholder: 'Pitch Type',
-    values: [
-      {
-        name: 'Four-seam Fastball',
-        value: 'fastball_4',
-      },
-      {
-        name     : 'Two-seam Fastball',
-        value    : 'fastball_2'
-      },
-      {
-        name     : 'Curveball',
-        value    : 'curveball'
-      },
-      {
-        name     : 'Slider',
-        value    : 'slider'
-      },
-      {
-        name     : 'Changeup',
-        value    : 'changeup'
-      }, 
-      {
-        name     : 'Other',
-        value    : 'other_pitch'
-      }
-    ]
-  })});
+
+$(document).ready(function () {
+    $('#pitch_type').dropdown({
+                                  action: function pitchDetailModal(text, value) {
+                                      $('#pitch_detail_modal').modal('show');
+                                  },
+                                  placeholder: 'Pitch Type',
+                                  values: [
+                                      {
+                                          name: 'Four-seam Fastball',
+                                          value: 'fastball_4'
+                                      },
+                                      {
+                                          name: 'Two-seam Fastball',
+                                          value: 'fastball_2'
+                                      },
+                                      {
+                                          name: 'Curveball',
+                                          value: 'curveball'
+                                      },
+                                      {
+                                          name: 'Slider',
+                                          value: 'slider'
+                                      },
+                                      {
+                                          name: 'Changeup',
+                                          value: 'changeup'
+                                      },
+                                      {
+                                          name: 'Other',
+                                          value: 'other_pitch'
+                                      }
+                                  ]
+                              });
+});
+
+
+function showPitchLocationModal(text, value) {
+    $('#pitch_location_modal').modal('show');
+}
+
+document.getElementById('#ball_top_left').click(function () {
+    alert('Handler for .click() called.');
+});
