eecbb5d935d149e37209b2b1b015642f301cb4de
bknum9 <92531634+bknum9@users.noreply.github.com>
2021-11-21 16:09:39 -0500

    combined modal example and play.html

--- play.js
+++ play.js
@@ -1,27 +1,65 @@
-function incrementStrike(){
-    var element = document.getElementById('incrementStrike');
-    var value = element.innerHTML;
+function showModal() {
+  $('#pitch_type_modal').modal('show');
+}
 
-    ++value;
-    if (value == 3){
-      window.clearInterval('incrementStrike');
-      value = 0;
-    }
-    console.log(value);
-    document.getElementById('incrementStrike').innerHTML = value;
-  }
-  function incrementBall(){
-    var element = document.getElementById('incrementBall');
-    var value = element.innerHTML;
 
-    ++value;
-    if (value == 4){
-      window.clearInterval('incrementBall');
-      value = 0;
-    }
-    console.log(value);
-    document.getElementById('incrementBall').innerHTML = value;
-  }
-  function content(elem) {
-    elem.style.backgroundColor = "red";
-  }
+$(document).ready(function () {
+  $('#pitch_type').dropdown({
+                                action: function pitchDetailModal(text, value) {
+                                    $('#pitch_detail_modal').modal('show');
+                                },
+                                placeholder: 'Pitch Type',
+                                values: [
+                                    {
+                                        name: 'Four-seam Fastball',
+                                        value: 'fastball_4'
+                                    },
+                                    {
+                                        name: 'Two-seam Fastball',
+                                        value: 'fastball_2'
+                                    },
+                                    {
+                                        name: 'Curveball',
+                                        value: 'curveball'
+                                    },
+                                    {
+                                        name: 'Slider',
+                                        value: 'slider'
+                                    },
+                                    {
+                                        name: 'Changeup',
+                                        value: 'changeup'
+                                    },
+                                    {
+                                        name: 'Other',
+                                        value: 'other_pitch'
+                                    }
+                                ]
+                            });
+});
+
+
+function showPitchLocationModal(text, value) {
+  $('#pitch_location_modal').modal('show');
+}
+
+// document.getElementById('#ball_top_left').click(function () {
+//     alert('Handler for .click() called.');
+// });
+
+$(document).ready(function () {
+  $('.menu .item').tab({history: false});
+});
+$(document).ready(function () {
+  $('#position_dropdown')
+      .dropdown()
+  ;
+});
+
+$(document).ready(function () {
+  var lineup_edit_link = document.getElementById('lineup_edit');
+  
+  lineup_edit_link.onclick = function () {
+      $('#edit_lineup_modal').modal('show');
+  };
+});
